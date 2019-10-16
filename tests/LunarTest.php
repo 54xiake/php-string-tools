94c71001ce3d93a25600302ec416e67e5854b233
yugang <yugang@chehang168.com>
2019-10-16 15:29:44 +0800

    demo

--- tests/LunarTest.php
+++ tests/LunarTest.php
@@ -5,16 +5,10 @@ use Xkcms\String\Lunar;
 
 class LunarTest extends TestCase
 {
-
-    function __construct()
-    {
-
-    }
-
     function testLunar()
     {
         $lunar = new Lunar();
         $month = $lunar->convertSolarToLunar('2019', '10', '16');//将阳历转换为阴历
-        $this->assertEquals('x', 'x');
+        $this->assertEquals($month[6], '猪');
     }
 }
